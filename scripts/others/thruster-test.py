82e2fea0e726e97dda57513a3e53446b14c62718
Piyush Mishra <piyushmishra0207@gmail.com>
2026-02-16 12:59:07 +0530

    chore: ros2 migration

--- scripts/others/thruster-test.py
+++ scripts/others/thruster-test.py
@@ -1,54 +1,62 @@
 #!/usr/bin/env python3
-import rospy
+import rclpy
+from rclpy.node import Node
 from std_msgs.msg import Int32MultiArray
 import blessings
 from sshkeyboard import listen_keyboard, stop_listening
 
-PWM_OFF = 1472
+PWM_OFF = 1500
 PWM_ON = 1600
-NUM_THRUSTERS = 6
+NUM_THRUSTERS = 7
 
-term = blessings.Terminal()
+class PWMTester(Node):
+    def __init__(self):
+        super().__init__('pwm_tester')
+        self.pub = self.create_publisher(Int32MultiArray, '/control/pwm', 10)
+        self.term = blessings.Terminal()
 
-pub = rospy.Publisher('/control/pwm', Int32MultiArray, queue_size=10)
-msg = Int32MultiArray()
+    def test_thruster(self, thruster: int):
+        msg = Int32MultiArray()
+        data = [PWM_OFF] * NUM_THRUSTERS
+        
+        # If a valid thruster index is provided, turn it ON
+        if 1 <= thruster <= NUM_THRUSTERS:
+            data[thruster - 1] = PWM_ON
+        
+        msg.data = data
+        self.pub.publish(msg)
 
+def main(args=None):
+    rclpy.init(args=args)
+    tester = PWMTester()
 
-def test_thruster(thruster: int):
-    data = [PWM_OFF] * NUM_THRUSTERS
-    
-    if thruster:
-        data[thruster - 1] = PWM_ON
-    
-    msg.data = data
-    pub.publish(msg)
+    print(tester.term.fullscreen())
+    print(tester.term.clear())
+    print(f"Press (1-{NUM_THRUSTERS}) to test thruster. Q to exit.\n")
 
+    def on_press(key):
+        if key == 'q':
+            stop_listening()
+        elif key.isdigit() and 1 <= int(key) <= NUM_THRUSTERS:
+            print(f"\nTesting thruster {int(key)}.")
+            tester.test_thruster(int(key))
+        else:
+            print("\nStopping thrusters.")
+            tester.test_thruster(0)
 
-def on_press(key):
-    if key == 'q':
-        stop_listening()
+    def on_release(key):
+        tester.test_thruster(0)
 
-    if key.isdigit() and 1 <= int(key) <= NUM_THRUSTERS:
-        print(f"\nTesting thruster {int(key)}.")
-        test_thruster(int(key))
-    else:
-        print("Stopping thrusters.")
-        test_thruster(0)
-
-
-def on_release(key):
-    print("Stopping thrusters.")
-    test_thruster(0)
-
+    try:
+        listen_keyboard(on_press=on_press, on_release=on_release)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Clean up
+        tester.test_thruster(0)
+        print(tester.term.clear())
+        tester.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
-    print(term.fullscreen())
-    print(term.clear())
-
-    rospy.init_node('pwm_tester', anonymous=True)
-    print(f"Press (1-{NUM_THRUSTERS}) to test thruster. Q to exit.\n")
-    
-    listen_keyboard(on_press=on_press, on_release=on_release)
-
-    print(term.clear())
-    exit(0)
+    main()
